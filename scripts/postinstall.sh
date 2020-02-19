ce00fce15d6f8cdb0d7ddea980b71b1971412200
Linus Klingzell <linus.klingzell@konsult.folksam.se>
2020-02-19 14:41:50 +0100

    feat(windows support): rewrite postinstall.sh to postinstall.js

--- scripts/postinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ ! -f "${INIT_CWD}/jsconfig.json" ]; then
-  echo '{\n  "compilerOptions": {\n    "target": "es6",\n    "module": "commonjs",\n    "resolveJsonModule": true\n  }\n}' >"${INIT_CWD}/jsconfig.json"
-fi
